524314e9eb566a8332499cab682fb842515c7c83
taddteps-creator <taddtep.s@email.com>
2026-02-14 22:36:40 +0700

    pepo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base : ['/reactweb101/'],
+  base: '/reactweb102/',
   plugins: [react()],
 })
