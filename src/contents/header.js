217b0da7a70a719aab0bba5e098944dab0b1aaaa
taddteps-creator <taddtep.s@email.com>
2026-02-16 00:38:14 +0700

    pepo3

--- src/contents/header.js
+++ src/contents/header.js
@@ -1,7 +1,7 @@
 import fileName from '../assets/cv.pdf';
 
 export const data ={
-    "name": "Suriya Pinitkan",
+    "name": "Tadtep Sae-jang",
     "title": "Software Developer · between jobs",
     "caption": "From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.",
     btnText: "View Resume",
